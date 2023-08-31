68338f1cf38bbb200f3b900da89023dbabfc5d0f
Jakub <daicheq@daicheq.pl>
2023-08-31 19:01:57 +0200

    logo now is visible in mobile and burgerBtn is remade

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
 const navMobileItems = document.querySelector('.nav-mobile-items')
 const burgerBtn = document.querySelector('.nav-mobile-btn')
+const body = document.querySelector('body')
 
 const showMobileMenu = () => {
 	navMobileItems.classList.toggle('nav-mobile-items-active')
+	if (body.style.overflow === 'visible') {
+		body.style.overflow = 'hidden'
+	} else {
+		body.style.overflow = 'visible'
+	}
 }
 
 burgerBtn.addEventListener('click', showMobileMenu)
+
+// TODO
+// DODAĆ chowanie menu po kliku nas start i w nav itemach
