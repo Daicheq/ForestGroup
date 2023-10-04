314a35e574769e1517911c878ac20ac36d65d517
Jakub <daicheq@daicheq.pl>
2023-10-04 19:37:25 +0200

    scroll spy is done, need to test

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const navLogo = document.querySelector('.nav__logo')
 const burgerBtn = document.querySelector('.nav__mobile-btn')
 const body = document.querySelector('body')
 const yearSpan = document.querySelector('.year-span')
+const sections = document.querySelectorAll('section')
+const navLinksDesktop = document.querySelectorAll('.nav__desktop-items .nav-item')
 
 const handleNav = () => {
 	if (body.style.overflow === 'visible') {
@@ -39,6 +41,38 @@ const handleYear = () => {
 	yearSpan.textContent = currentYear
 }
 
+const handleScrollSpy = () => {
+	const scrollY = window.scrollY || window.pageYOffset
+
+	if (scrollY === 0) {
+		const homeLink = document.querySelector(`.nav__desktop-items .nav-item[href="#"]`)
+		navLinksDesktop.forEach(link => {
+			link.classList.remove('active')
+		})
+		homeLink.classList.add('active')
+	} else {
+		sections.forEach(sec => {
+			let top = window.scrollY
+			let offset = sec.offsetTop - 150
+			let height = sec.offsetHeight
+			let id = sec.getAttribute('id')
+
+			if (top >= offset && top < offset + height) {
+				navLinksDesktop.forEach(link => {
+					link.classList.remove('active')
+				})
+
+				const correspondingLink = document.querySelector(`.nav__desktop-items .nav-item[href="#${id}"]`)
+
+				if (correspondingLink) {
+					correspondingLink.classList.add('active')
+				}
+			}
+		})
+	}
+}
+
+document.addEventListener('scroll', handleScrollSpy)
 burgerBtn.addEventListener('click', handleNav)
 burgerBtn.addEventListener('click', handleBurgerBtnAnimation)
 document.addEventListener('DOMContentLoaded', handleYear)
